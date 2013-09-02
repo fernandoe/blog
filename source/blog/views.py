edb31df01162860d53acb124983c1a5d55ef4eb6
Fernando Espíndola <fer.esp@gmail.com>
2013-09-02 19:26:53 -0300

    Ajustes nas categorias

--- source/blog/views.py
+++ source/blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from blog.models import Post
+from blog.models import Post, Category
 
 
 class BlogListView(ListView):
@@ -13,19 +13,32 @@ class BlogListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(BlogListView, self).get_context_data(**kwargs)
         context['menu'] = 'blog' 
+        context['categories'] = Category.objects.all()
         return context
 
     def get_queryset(self):
-        return Post.objects.filter(published=True)
+        posts = Post.objects.filter(published=True)
+        category = self.request.GET.get('category', None)
+        if category:
+            category = Category.objects.get(name=category)
+            posts = posts.filter(categories__name=category)
+        return posts
 
 
 class PostTemplateView(DetailView):
     template_name = "blog/post.html"
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super(PostTemplateView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['menu'] = None
+        return context
+
 
 class AboutTemplateView(TemplateView):
     template_name = "blog/about.html"
+
     def get_context_data(self, **kwargs):
         context = super(AboutTemplateView, self).get_context_data(**kwargs)
         context['menu'] = 'about' 
